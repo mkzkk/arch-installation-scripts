ea72eec1351ade8304038af61b0908cc893d4d6b
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-08-24 07:52:21 -0600

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
+
+if ! ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
+    echo -e "\nNo internet connectivity detected\nConnect to a network and try again\nAborting installer...\n"
+    exit 0
+fi
+
 pass="$1"
 
 if [[ -z "$pass" ]] ; then
     echo ; read -p 'Enter installer password: ' password
     echo
-    curl --user "zkkm@pm.me:${password}" 'http://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh' > arch.sh
+    curl -O --user "zkkm@pm.me:${password}" 'http://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh'
 else
     echo
-    curl --user "zkkm@pm.me:${pass}" 'http://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh' > arch.sh
+    curl -O --user "zkkm@pm.me:${pass}" 'http://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh'
 fi
 
 echo
 while grep -q "Username or password was incorrect" arch.sh ; do
     echo "Incorrect password, try again"
-    if [[ -z "$pass" ]] ; then
-        echo ; read -p 'Enter installer password: ' password
-        echo
-        curl --user "zkkm@pm.me:${password}" 'http://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh' > arch.sh
-    else
-        echo
-        exit 0
-    fi
+    echo ; read -p 'Enter installer password: ' password
+    echo
+    curl --user "zkkm@pm.me:${password}" 'http://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh'
 done
 
 sh arch.sh

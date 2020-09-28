b4a450584241f4318d1865926240b998cb7fabfd
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-09-28 06:59:09 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
-if ! ping -q -c 1 -W 1 8.8.8.8 >/dev/null; then
-    echo -e "\nNo internet connectivity detected\nConnect to a network and try again\nAborting installer...\n"
+! ping -q -c 1 -W 1 8.8.8.8 > /dev/null &&
+    echo -e "\nNo internet connectivity detected\nConnect to a network and try again\nAborting installer...\n" &&
     exit 0
-fi
 
-pass="$1"
-
-if [[ -z "$pass" ]] ; then
-    read -p 'Enter installer password: ' password
-    echo
-    curl -O --user "zkkm@pm.me:${password}" 'https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh'
-else
-    echo
-    curl -O --user "zkkm@pm.me:${pass}" 'https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh'
-fi
+[ "$1" ] &&
+    echo &&
+    curl -O --user "zkkm@pm.me:$1" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh"
+[ -z "$1" ] &&
+    read -p "Enter installer password: " password &&
+    echo &&
+    curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh"
 
 echo
+    
 while grep -q "Username or password was incorrect" arch.sh ; do
-    echo ; echo "Incorrect password, try again"
-    read -p 'Enter installer password: ' password
+    echo -e "\nIncorrect password, try again"
+    read -p "Enter installer password: " password
     echo
-    curl -O --user "zkkm@pm.me:${password}" 'https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh'
+    curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh"
 done
 
 sh arch.sh
-

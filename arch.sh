4e4e074b4e698e90d4f9ba24c3482ee37acb8a74
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-09-30 05:24:49 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 ! ping -q -c 1 -W 1 8.8.8.8 > /dev/null &&
-    echo -e "\nNo internet connectivity detected\nConnect to a network and try again\nAborting installer...\n" &&
+    echo -e "\nNo internet connectivity detected" &&
+    echo -e "Connect to a network and try again\nAborting installer...\n" &&
     exit 0
 
 if [ "$*" ]; then
     echo
     curl -O --user "zkkm@pm.me:$1" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh" || curl -O --user "zkkm@pm.me:$1" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh"
-elif [ -z "$*" ]; then
+else
     read -p "Enter installer password: " password
     echo
     curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/arch.sh" || curl -O --user "zkkm@pm.me:$password" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/arch.sh"

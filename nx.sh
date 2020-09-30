9a77c99685662a402704c7e7bde09f0f3aaa4231
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-09-30 05:25:34 +0000

    Update nx.sh

--- nx.sh
+++ nx.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 ! ping -q -c 1 -W 1 8.8.8.8 > /dev/null &&
-    echo -e "\nNo internet connectivity detected\nConnect to a network and try again\nAborting installer...\n" &&
+    echo -e "\nNo internet connectivity detected" &&
+    echo -e "Connect to a network and try again\nAborting installer...\n" &&
     exit 0
 
-if [ "$*" ] ; then
+if [ "$*" ]; then
     echo
-    curl -O --user "zkkm@pm.me:$1" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/nx.sh"
-elif [ -z "$*" ] ; then
+    curl -O --user "zkkm@pm.me:$1" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/nx.sh" || curl -O --user "zkkm@pm.me:$1" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/nx.sh"
+else
     read -p "Enter installer password: " password
     echo
-    curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/nx.sh"
+    curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/nx.sh" || curl -O --user "zkkm@pm.me:$password" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/nx.sh"
 fi
 
 echo
     
-while grep -q "Username or password was incorrect" nx.sh ; do
+while grep -q "Username or password was incorrect" arch.sh ; do
     echo -e "\nIncorrect password, try again"
     read -p "Enter installer password: " password
     echo
-    curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/nx.sh"
+    curl -O --user "zkkm@pm.me:$password" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/nx.sh" || curl -O --user "zkkm@pm.me:$password" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/nx.sh"
 done
 
-sh nx.sh
+sh arch.sh

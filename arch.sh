a89957b6635649c9a16665583d8e23ab1b755476
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-10-05 07:19:08 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -2,7 +2,7 @@
 ! ping -q -c 1 -W 1 8.8.8.8 > /dev/null &&
     echo -e "\nNo internet connectivity detected" &&
     echo -e "Connect to a network and try again" &&
-    echo -e Aborting installer...\n" &&
+    echo -e "Aborting installer...\n" &&
     exit 0
 
 if [ "$*" ]; then

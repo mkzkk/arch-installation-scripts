40307a5ff38b23a834d4d80f8aa7b53a84d32914
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-10-28 10:10:28 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -6,7 +6,7 @@ if [ "$*" ]; then
     elif echo "$1" | grep -q "n[^ ]*d\|nx"; then
         SCRIPT='nx.sh'
     elif echo "$1" | grep -q "u[^ ]*b\|r[^ ]*r\|r[^ ]*y"; then
-        SCRIPT='liveusbrecovery.sh'
+        SCRIPT='recovery.sh'
     else
         echo -e "\n\e[1;36mSelect a script to run\e[0m"
         echo -e "\t1) Arch Installer"
@@ -18,7 +18,7 @@ if [ "$*" ]; then
             done ; echo
         [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
         [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='liveusbrecovery.sh'
+        [ "$SCRIPT" = 3 ] && SCRIPT='recovery.sh'
     fi
     echo
     curl -O --user "zkkm@pm.me:$2" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$2" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
@@ -33,7 +33,7 @@ else
             done
         [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
         [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='liveusbrecovery.sh'
+        [ "$SCRIPT" = 3 ] && SCRIPT='recovery.sh'
     read -rp $'\n\n\e[1;36mEnter installer password: \e[0m' PASSWORD
     echo    
     curl -O --user "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"

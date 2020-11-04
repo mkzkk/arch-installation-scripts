73b8b8391e50ee383ff5b1b153cd203a0d7942b6
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-11-04 03:55:39 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
 if [ "$*" ]; then
-    if echo "$1" | grep -q "a[^ ]*h"; then
+    if grep -q "a[^ ]*h" <<< "$1"; then
         SCRIPT='arch.sh'
-    elif echo "$1" | grep -q "n[^ ]*d\|nx"; then
+    elif grep -q "n[^ ]*d\|nx" <<< "$1"; then
         SCRIPT='nx.sh'
-    elif echo "$1" | grep -q "u[^ ]*b\|r[^ ]*r\|r[^ ]*y"; then
-        SCRIPT='recovery.sh'
+    elif grep -q "u[^ ]*b\|r[^ ]*e" <<< "$1"; then
+        SCRIPT='rescue.sh'
     else
         echo -e "\n\e[1;36mSelect a script to run\e[0m"
         echo -e "\t1) Arch Installer"
         echo -e "\t2) Nextcloud Server Installer"
-        echo -e "\t3) Live USB Recovery"
+        echo -e "\t3) Rescue USB"
             until [[ "$SCRIPT" = [123] ]]; do
                 read -n1 -p '> ' SCRIPT
                     [[ "$SCRIPT" = [123] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 3\e[0m"
             done ; echo
         [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
         [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='recovery.sh'
+        [ "$SCRIPT" = 3 ] && SCRIPT='rescue.sh'
     fi
     echo
     curl -O --user "zkkm@pm.me:$2" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$2" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
@@ -33,13 +33,13 @@ else
             done
         [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
         [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='recovery.sh'
+        [ "$SCRIPT" = 3 ] && SCRIPT='rescue.sh'
     read -rp $'\n\n\e[1;36mEnter installer password: \e[0m' PASSWORD
-    echo    
+    echo
     curl -O --user "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
 fi
 
-while grep -q "Username or password was incorrect" $SCRIPT ; do
+while ! grep -q '#!/bin/bash' $SCRIPT ; do
     echo -e "\n\e[1;31mIncorrect password, try again\e[0m"
     read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
     echo

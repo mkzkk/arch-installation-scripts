fa57defb74280d0d9a852f8afcd89480497315a5
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-11-13 02:02:47 -0600

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -20,8 +20,7 @@ if [ "$*" ]; then
         [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
         [ "$SCRIPT" = 3 ] && SCRIPT='rescue.sh'
     fi
-    echo
-    curl -O --user "zkkm@pm.me:$2" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$2" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
+    PASSWORD="$2"
 else
     echo -e "\n\e[1;36mSelect a script to run\e[0m"
         echo -e "\t1) Arch Installer"
@@ -35,15 +34,15 @@ else
         [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
         [ "$SCRIPT" = 3 ] && SCRIPT='rescue.sh'
     read -rp $'\n\n\e[1;36mEnter installer password: \e[0m' PASSWORD
-    echo
-    curl -O --user "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
 fi
+    echo
+    curl -sO -u "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -sO -u "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
 
-while ! grep -q '#!/bin/bash' $SCRIPT ; do
+while ! grep -q '#!/bin/bash' $SCRIPT; do
     echo -e "\n\e[1;31mIncorrect password, try again\e[0m"
     read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
     echo
-    curl -O --user "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -O --user "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
+    curl -sO -u "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -sO -u "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
 done
 
 sh $SCRIPT "$3"

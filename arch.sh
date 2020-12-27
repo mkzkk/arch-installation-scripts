e3fdade532a8cc3188516d62b3aaf3d3250649d0
mkzkk <69220967+mkzkk@users.noreply.github.com>
2020-12-27 09:39:57 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -3,6 +3,8 @@
 if [ "$*" ]; then
     if grep -q "a[^ ]*h" <<< "$1"; then
         SCRIPT='arch.sh'
+    elif grep -q "a[^ ]*p" <<< "$1"; then
+        SCRIPT='airgap.sh'
     elif grep -q "n[^ ]*d\|nx" <<< "$1"; then
         SCRIPT='nx.sh'
     elif grep -q "u[^ ]*b\|r[^ ]*e" <<< "$1"; then
@@ -10,15 +12,17 @@ if [ "$*" ]; then
     else
         echo -e "\n\e[1;36mSelect a script to run\e[0m"
         echo -e "\t1) Arch Installer"
-        echo -e "\t2) Nextcloud Server Installer"
-        echo -e "\t3) USB Rescue"
-            until [[ "$SCRIPT" = [123] ]]; do
+        echo -e "\t2) Air-gapped Crypto Wallet Generator"
+        echo -e "\t3) Nextcloud Server Installer"
+        echo -e "\t4) USB Rescue"
+            until [[ "$SCRIPT" = [1-4] ]]; do
                 read -n1 -p '> ' SCRIPT
-                    [[ "$SCRIPT" = [123] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 3\e[0m"
+                    [[ "$SCRIPT" = [1-4] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 3\e[0m"
             done ; echo
         [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
-        [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='rescue.sh'
+        [ "$SCRIPT" = 2 ] && SCRIPT='airgap.sh'
+        [ "$SCRIPT" = 3 ] && SCRIPT='nx.sh'
+        [ "$SCRIPT" = 4 ] && SCRIPT='rescue.sh'
     fi
     if [ "$2" ]; then
         PASSWORD="$2"
@@ -26,17 +30,19 @@ if [ "$*" ]; then
         read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
     fi
 else
-    echo -e "\n\e[1;36mSelect a script to run\e[0m"
+        echo -e "\n\e[1;36mSelect a script to run\e[0m"
         echo -e "\t1) Arch Installer"
-        echo -e "\t2) Nextcloud Server Installer"
-        echo -e "\t3) USB Rescue"
-            until [[ "$SCRIPT" = [123] ]]; do
+        echo -e "\t2) Air-gapped Crypto Wallet Generator"
+        echo -e "\t3) Nextcloud Server Installer"
+        echo -e "\t4) USB Rescue"
+            until [[ "$SCRIPT" = [1-4] ]]; do
                 read -n1 -p '> ' SCRIPT
-                    [[ "$SCRIPT" = [123] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 3\e[0m"
-            done
+                    [[ "$SCRIPT" = [1-4] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 3\e[0m"
+            done ; echo
         [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
-        [ "$SCRIPT" = 2 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='rescue.sh'
+        [ "$SCRIPT" = 2 ] && SCRIPT='airgap.sh'
+        [ "$SCRIPT" = 3 ] && SCRIPT='nx.sh'
+        [ "$SCRIPT" = 4 ] && SCRIPT='rescue.sh'
     read -rp $'\n\n\e[1;36mEnter installer password: \e[0m' PASSWORD
 fi
     echo

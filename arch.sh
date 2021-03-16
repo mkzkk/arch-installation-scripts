4a4acd6924a36638d815a64aaf1368b287d7417a
mkzkk <69220967+mkzkk@users.noreply.github.com>
2021-03-16 06:54:40 +0000

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,58 +1,59 @@
-#!/bin/sh
+#!/bin/bash
 
-if [ "$*" ]; then
-    if grep -q "a[^ ]*h" <<< "$1"; then
-        SCRIPT='arch.sh'
-    elif grep -q "a[^ ]*p" <<< "$1"; then
-        SCRIPT='airgap.sh'
-    elif grep -q "n[^ ]*d\|nx" <<< "$1"; then
-        SCRIPT='nx.sh'
-    elif grep -q "u[^ ]*b\|r[^ ]*e" <<< "$1"; then
-        SCRIPT='rescue.sh'
-    else
-        echo -e "\n\e[1;36mSelect a script to run\e[0m"
-        echo -e "\t1) Arch Installer"
-        echo -e "\t2) Air-gapped Crypto Wallet Generator"
-        echo -e "\t3) Nextcloud Server Installer"
-        echo -e "\t4) USB Rescue"
-            until [[ "$SCRIPT" = [1-4] ]]; do
-                read -n1 -p '> ' SCRIPT
-                    [[ "$SCRIPT" = [1-4] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 4\e[0m"
-            done ; echo
-        [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
-        [ "$SCRIPT" = 2 ] && SCRIPT='airgap.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 4 ] && SCRIPT='rescue.sh'
+# Detect network connectivity
+if ! ping -qc1 -W3 8.8.8.8 > /dev/null; then
+    echo -e '\n\t\e[31mNo internet connection detected, run \e[1;35mnmtui\e[0m\e[31m in the terminal if connecting through wifi\e[0m\n'
+    exit 1
+fi
+
+# Function to list all choices
+LIST_SCRIPTS() {
+    echo -e '\n\e[1;36mSelect a script to run\e[0m'
+    echo -e '\t1) Arch Installer'
+    echo -e '\t2) Air-gapped Crypto Wallet Generator'
+    echo -e '\t3) Nextcloud Server Installer'
+    echo -e '\t4) USB Rescue'
+        until [[ "$SCRIPT" = [1-4] ]]; do
+            read -n1 -p '> ' SCRIPT
+                [[ "$SCRIPT" = [1-4] ]] || echo -e '\n\n\e[1;31mInvalid selection, type an option from 1 to 4\e[0m'
+        done ; echo
+    [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
+    [ "$SCRIPT" = 2 ] && SCRIPT='airgap.sh'
+    [ "$SCRIPT" = 3 ] && SCRIPT='nx.sh'
+    [ "$SCRIPT" = 4 ] && SCRIPT='rescue.sh'
+}
+# Function to download script from webdav server
+DOWNLOAD_SCRIPT() { echo
+    curl -sO -u "scripts:$PASSWORD" "http://70.64.30.185:8080/remote.php/dav/files/scripts/$SCRIPT"
+    if ! grep -q '#!/bin/bash' $SCRIPT; then
+        curl -sO -u "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
     fi
-    if [ "$2" ]; then
-        PASSWORD="$2"
-    else
-        read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
+}
+
+# Detect script choice and prompt for webdav password
+if [ "$*" ]; then
+    # Assign "$1" as script name
+    if grep -q "a[^ ]*h" <<< "$1"; then SCRIPT='arch.sh'
+    elif grep -q "a[^ ]*p" <<< "$1"; then SCRIPT='airgap.sh'
+    elif grep -q "n[^ ]*d\|nx" <<< "$1"; then SCRIPT='nx.sh'
+    elif grep -q "u[^ ]*b\|r[^ ]*e" <<< "$1"; then SCRIPT='rescue.sh'
+    else LIST_SCRIPTS
+    fi
+    # Assign "$2" as password if present
+    if [ "$2" ]; then PASSWORD="$2"
+    else read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
     fi
 else
-        echo -e "\n\e[1;36mSelect a script to run\e[0m"
-        echo -e "\t1) Arch Installer"
-        echo -e "\t2) Air-gapped Crypto Wallet Generator"
-        echo -e "\t3) Nextcloud Server Installer"
-        echo -e "\t4) USB Rescue"
-            until [[ "$SCRIPT" = [1-4] ]]; do
-                read -n1 -p '> ' SCRIPT
-                    [[ "$SCRIPT" = [1-4] ]] || echo -e "\n\n\e[1;31mInvalid selection, type an option from 1 to 4\e[0m"
-            done ; echo
-        [ "$SCRIPT" = 1 ] && SCRIPT='arch.sh'
-        [ "$SCRIPT" = 2 ] && SCRIPT='airgap.sh'
-        [ "$SCRIPT" = 3 ] && SCRIPT='nx.sh'
-        [ "$SCRIPT" = 4 ] && SCRIPT='rescue.sh'
-    read -rp $'\n\n\e[1;36mEnter installer password: \e[0m' PASSWORD
-fi
-    echo
-    curl -sO -u "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -sO -u "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
-
-while ! grep -q '#!/bin/bash' $SCRIPT; do
-    echo -e "\t\e[1;31mIncorrect password, try again\e[0m"
+    LIST_SCRIPTS
     read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
-    echo
-    curl -sO -u "zkkm@pm.me:$PASSWORD" "https://shared02.opsone-cloud.ch/remote.php/dav/files/zkkm@pm.me/$SCRIPT" || curl -sO -u "zkkm@pm.me:$PASSWORD" "https://us.cloudamo.com/remote.php/dav/files/zkkm@pm.me/$SCRIPT"
+fi
+
+DOWNLOAD_SCRIPT
+
+while ! grep -q '#!/bin/bash' "$SCRIPT"; do
+    echo -e '\t\e[1;31mIncorrect password, try again\e[0m'
+    read -rp $'\n\e[1;36mEnter installer password: \e[0m' PASSWORD
+    DOWNLOAD_SCRIPT
 done
 
-sh $SCRIPT "$3"
+sh "$SCRIPT"
